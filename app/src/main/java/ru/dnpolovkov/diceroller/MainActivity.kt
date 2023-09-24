566397a02a480f01f98774d36d7570a346fca195
Denis Polovkov <denis.polovkoff@gmail.com>
2023-09-24 16:12:45 +0300

    Add updates the TextView when the Button is clicked

--- app/src/main/java/ru/dnpolovkov/diceroller/MainActivity.kt
+++ app/src/main/java/ru/dnpolovkov/diceroller/MainActivity.kt
@@ -2,7 +2,7 @@ package ru.dnpolovkov.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -12,10 +12,8 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton: Button = findViewById(R.id.button)
         rollButton.setOnClickListener {
-            val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
-            toast.show()
-            // or this
-            // val toast =  Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT).show()
+            val resultTextView: TextView = findViewById(R.id.textView)
+            resultTextView.text = "6"
         }
     }
 }
